7ef74264a1e45f081dfdebdaedeb61531cdbc503
aach1267 <aanchalsharmax1@gmail.com>
2025-12-02 00:05:30 +0530

    Fix:some changes due to pagination

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -6,25 +6,29 @@ import { useNavigate } from "react-router-dom";
 import { FaSort } from "react-icons/fa";
 import Tables from "../components/tables/Tables";
 import Spiner from "../components/spiner/Spiner";
-import {UserContext} from "../components/context/ContextProvider";
+import { UserContext } from "../components/context/ContextProvider";
 import Alert from "react-bootstrap/Alert";
 import { usergetfunc, deletefunc } from "../services/Apis";
-// import InfiniteScroll from "react-infinite-scroll-component";
-
-
+import { useSearchParams } from "react-router-dom";
 
 function Home() {
   const [showspin, setShowSpin] = useState(true);
   const [allusers, setAllUsers] = useState([]);
-  const [search, setSearch] = useState("");
-  const [gender, setGender] = useState("All");
-  const [status, setStatus] = useState("All");
-  const [sort, setSort] = useState("new")
+  const [totalPages, setTotalPages] = useState(1);
+
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const navigate = useNavigate();
 
+  const [search, setSearch] = useState(searchParams.get("search") || "");
+  const [gender, setGender] = useState(searchParams.get("gender") || "All");
+  const [status, setStatus] = useState(searchParams.get("status") || "All");
+  const [sort, setSort] = useState(searchParams.get("sort") || "new");
+  const [page, setPage] = useState(parseInt(searchParams.get("page")) || 1);
+
   //alerts
-  const {useradd, setUserAdd, update, setUpdate, delUser, setDelUser} = useContext(UserContext)
+  const { useradd, setUserAdd, update, setUpdate, delUser, setDelUser } =
+    useContext(UserContext);
 
   console.log(useradd);
   const adduser = () => {
@@ -34,11 +38,12 @@ function Home() {
   // getallusers
   const getallUsers = async () => {
     //search
-    const res = await usergetfunc(search, gender, status, sort);
+    const res = await usergetfunc(search, gender, status, sort, page);
 
     if (res.status === 200) {
-      console.log(res.data.users)
-      setAllUsers(res.data.users)
+      console.log(res.data.users);
+      setAllUsers(res.data.users);
+      setTotalPages(res.data.total);
     }
   };
 
@@ -50,15 +55,28 @@ function Home() {
       setDelUser(true);
       getallUsers();
     }
-  }
- 
+  };
+  
+    // update URL query params whenever filters/search/page changes
+  useEffect(() => {
+    const params = { search, gender, status, sort, page };
+    
+    // remove default values 
+    if (gender === "All") delete params.gender;
+    if (status === "All") delete params.status;
+    if (sort === "new") delete params.sort;
+    // if (page === 1) delete params.page;
+    if (!search) delete params.search;
+
+    setSearchParams(params);
+  }, [search, gender, status, sort, page]);
 
   useEffect(() => {
-    getallUsers();
+    getallUsers()
     setTimeout(() => {
       setShowSpin(false);
     }, 1200);
-  }, [search, gender, status, sort]); // so that whenever serach getalluser changes as per search
+  }, [search, gender, status, sort, page]); // so that whenever serach getalluser changes as per search
 
   return (
     <>
@@ -91,12 +109,13 @@ function Home() {
             <Form.Control
               type="text"
               placeholder="search...."
+              value={search}    
               className="w-full"
               onChange={(e) => setSearch(e.target.value)}
             />
-            <Button variant="success" className="px-4 py-2">
+            {/* <Button variant="success" onClick={()=>setPage(1)} className="px-4 py-2">
               Search
-            </Button>
+            </Button> */}
           </Form>
           <div>
             <Button variant="primary" onClick={adduser}>
@@ -189,11 +208,16 @@ function Home() {
         {showspin ? (
           <Spiner />
         ) : (
-             <Tables
-            allusers={allusers}
-            deleteUser={deleteUser}
-            getallUsers={getallUsers}
+          <>
+            <Tables
+              allusers={allusers}
+              deleteUser={deleteUser}
+              getallUsers={getallUsers}
+              page={page}
+              totalPages={totalPages}
+              onPageChange={setPage}
             />
+          </>
         )}
       </div>
     </>
@@ -202,30 +226,40 @@ function Home() {
 
 export default Home;
 
+// useSearchParams se sab states (search, filter, sort, page) URL me 
+// reflect hote hain aur refresh ke baad bhi retain ho jate hain.
 
+//create function to load more
+// const getMoreUser = async()=>{
+//  const nextPage = page ? page + 1 : 1
 
+//  const res = await usergetfunc(search, gender, status, sort, nextPage)
+//   if(res.status == 200){
+//     if(res.data.users.length == 0){
+//       setHasMore(false)
+//       return
+//     }
+//        console.log(res)
+//     setAllUsers(prev => [...prev,...res.data.users]) // append users
+//     setPage(nextPage)
+//   }
+// }
+// <InfiniteScroll
+//           dataLength ={allusers.length}
+//           hasMore = {hasMore}
+//           next = {getMoreUser}
+//           loader = {<h4><Spiner/></h4>}
+//           endMessage = {<p className="textAlign:center">No more Users</p>}
+//          ></InfiniteScroll>
+////Home.jsx → API call + states
 
+// mistake 
+// error due to your search was not work due to not mention value attritube as value change to value aaye na value ={search}
+// dynamic search (onChange) no need button
 
-
-  //create function to load more 
-  // const getMoreUser = async()=>{
-  //  const nextPage = page ? page + 1 : 1
-
-  //  const res = await usergetfunc(search, gender, status, sort, nextPage)
-  //   if(res.status == 200){
-  //     if(res.data.users.length == 0){
-  //       setHasMore(false)
-  //       return
-  //     }
-  //        console.log(res)
-  //     setAllUsers(prev => [...prev,...res.data.users]) // append users
-  //     setPage(nextPage)
-  //   }
-  // }
-  // <InfiniteScroll
-  //           dataLength ={allusers.length}
-  //           hasMore = {hasMore} 
-  //           next = {getMoreUser}
-  //           loader = {<h4><Spiner/></h4>}
-  //           endMessage = {<p className="textAlign:center">No more Users</p>}
-  //          ></InfiniteScroll>
+// feature of  these project i add 
+// Dynamic search, sorting, filtering
+// Form validation (frontend + backend)
+// Global state management using Context API
+// CRUD operations: add, view, delete users
+// Image upload with Cloudinary
