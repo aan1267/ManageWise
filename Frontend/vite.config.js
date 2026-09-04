8dfcc9a070efcea35423311607a13f3f3f56e019
aach1267 <aanchalsharmax1@gmail.com>
2026-09-04 16:54:14 +0530

    fix eslint error

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite'
 import { fileURLToPath } from "url";
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 
 
